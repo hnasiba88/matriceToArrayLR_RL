9095ce86004aaddf6eff19f4decbdf6cccb41035
nasiba <nasiba.huseynova29@gmail.com>
2019-03-10 23:58:38 +0400

    Refactor

--- matrixToArrayLR_RL/src/matriceToArray/MatriceToArray.java
+++ matrixToArrayLR_RL/src/matriceToArray/MatriceToArray.java
@@ -1,10 +1,10 @@
-package matrixToArray;
+package matriceToArray;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class MatrixToArray {
+public class MatriceToArray {
 
 	static int m, n;
 	static Random rand = new Random();
@@ -18,19 +18,19 @@ public class MatrixToArray {
 		System.out.print("Size of input matrix n=");
 		sc = new Scanner(System.in);
 		n = sc.nextInt();
-		int matrix[][] = new int[m][n];
+		int matrice[][] = new int[m][n];
 		for (int row = 0; row < m; row++) {
 			for (int col = 0; col < n; col++) {
-				matrix[row][col] = rand.nextInt(10);
+				matrice[row][col] = rand.nextInt(10);
 			}
 		}
-		System.out.println(Arrays.deepToString(matrix));
+		System.out.println(Arrays.deepToString(matrice));
 		System.out.println("Choose direction please for reading:");
 		System.out.println("Type 0 for LR");
 		System.out.println("Type 1 for RL");
 		System.out.print("Your choice: ");
 		sc = new Scanner(System.in);
-		printArray(matrix, sc.nextInt());
+		printArray(matrice, sc.nextInt());
 	}
 
 	private static void printArray(int[][] arr, int direction) {
